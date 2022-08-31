6161a8b6832e3ed6efe659da003720bde091b89c
madisenwicker <madisenwicker@symmetryscienceconsulting.com>
2022-08-31 14:29:44 -0400

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Madisen Wicker on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
